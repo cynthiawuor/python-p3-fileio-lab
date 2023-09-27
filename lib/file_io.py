1fa336fc9172628aff1a7392d532f073b702d63d
cynthiawuor <cynthiawuor254@gmail.com>
2023-09-27 10:12:30 +0300

    filelio

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,32 @@
+# lib/file_io.py
+
 def write_file(file_name, file_content):
-    pass
+    file_name = str(file_name) + ".txt"
+    
+    try:
+        with open(file_name, "w") as file:
+            file.write(file_content)
+    except IOError as e:
+        print(f"Error writing to {file_name}: {e}")
 
 def append_file(file_name, append_content):
-    pass
+    file_name = str(file_name) + ".txt"
+    
+    try:
+        with open(file_name, "a") as file:
+            if not append_content.endswith("\n"):
+                append_content += "\n"  
+            file.write(append_content)
+    except IOError as e:
+        print(f"Error appending to {file_name}: {e}")
 
 def read_file(file_name):
-    pass
+    file_name = str(file_name) + ".txt"
+    
+    try:
+        with open(file_name, "r") as file:
+            content = file.read()
+            return content
+    except IOError as e:
+        print(f"Error reading from {file_name}: {e}")
+        return None
